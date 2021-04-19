609bff3a1f4e7cb71ea08672329b419bb729e9d9
alan santana <alan.nsantana@hotmail.com>
2021-04-19 16:44:32 -0300

    primeiro dia

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app =  express();
+
+app.get("/", (req, resp) =>{
+    return resp.json({
+        message: "Ola NLW 05"
+    });
+});
+
+app.post("/", (req, resp) =>{
+    return resp.json({
+        message: "Usuário salvo com sucesso!"
+    })
+})
+
+app.listen(3000, () => console.log("Servidor Voando"));
+
+//para rodar o servidor: yarn dev
