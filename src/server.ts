80f48cd1a1d16264f208b88761a1adac98221121
alan santana <alan.nsantana@hotmail.com>
2021-04-23 14:23:35 -0300

    configurando socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import express from "express";
+import {createServer} from "http";
+import {Server, Socket} from "socket.io";
 
 import "./database";
 import {routes} from "./routes";
 
 const app =  express();
 
+const http = createServer();
+const io = new Server(http);
+
+io.on("connection", (socket: Socket ) =>{
+    console.log("Se conectou", socket.id);
+})
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => console.log("Servidor Voando"));
+http.listen(3000, () => console.log("Servidor Voando"));
 
 //para rodar o servidor: yarn dev
